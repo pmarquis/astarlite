56f193e70da87872a7a9c6f49aa1a036a1444af0
Pierre Marquis <pier.marquis@gmail.com>
2014-10-12 05:15:37 -0700

    download OSM data path up-to-date

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget http://downloads.cloudmade.com/europe/western_europe/france/ile-de-france/ile-de-france.osm.bz2
+wget http://download.geofabrik.de/europe/france/ile-de-france-latest.osm.bz2
 bunzip2 ile-de-france.osm.bz2
 
